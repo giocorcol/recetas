3b3b4d6d28912cc3c8eccdbdd2955e27704dae94
Giovanni Cortes Colorado <giocorcol@gmail.com>
2024-08-07 08:20:45 -0500

    envia tu receta y avance modo oscuro

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const boton = document.querySelector("#dianoche1");
+const cambiofondobody = document.querySelector("body");
+const cambiorecta1 = document.querySelector(".receta1");
+const cambiorecta2 = document.querySelector(".receta2");
+const cambiorecta3 = document.querySelector(".receta3");
+const cambiorecta4 = document.querySelector(".receta4");
+const cambiorecta5 = document.querySelector(".receta5");
+const cambiorecta6 = document.querySelector(".receta6");
+const td2 = document.querySelector(".t2");//titulo de recetas en index
+const mainenviatureceta = document.querySelector(".mainmainenviatureceta");//envia tu receta main
+const linkrecetas1 = document.querySelectorAll(".linkrecetas");//link de recetas en index
+const lover = document.querySelector(".linkrecetas:hover");//link de recetas en index
+const a1 = document.querySelector(".artrecetas");
+
+
+
+boton.addEventListener("click", () => {
+    cambiofondobody.classList.toggle("temaoscuro");
+    cambiorecta1.classList.toggle ("receta1t");
+    cambiorecta2.classList.toggle ("receta2t");
+    cambiorecta3.classList.toggle ("receta3t");
+    cambiorecta4.classList.toggle ("receta4t");
+    cambiorecta5.classList.toggle ("receta5t");
+    cambiorecta6.classList.toggle ("receta6t");
+    td2.classList.toggle ("t2tema");//titulo de recetas en index
+    mainenviatureceta.classList.toggle ("maintema");//envia tu receta main
+    linkrecetas1.classList.toggle ("linkrecetastema");//link de recetas en index
+    lover.classList.toggle ("linkrecetastema:hover");//link de recetas en index
+    a1.classList.toggle ("artrecetastema");
+     
+
+})
